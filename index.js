f2b45eb801cdcebfcbfa7e6d714e83513012f639
wu529778790 <529778790@qq.com>
2023-07-23 20:16:37 +0800

    chore: 调整结构

--- index.js
+++
@@ -1,8 +0,0 @@
-// 启动脚本.js
-
-const concurrently = require('concurrently');
-
-concurrently([
-  { command: 'cd frontend && npm run dev', name: 'frontend' },
-  { command: 'cd backend && npm run start:dev', name: 'backend' },
-]);
