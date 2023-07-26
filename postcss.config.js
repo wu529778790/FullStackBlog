acd2a2141f589ac4fa0be8539638976eda0e1ec7
wu529778790 <529778790@qq.com>
2023-07-27 00:47:08 +0800

    Added postcss-px-to-viewport plugin to package.json and created postcss.config.js file.

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: {
+        // 'postcss-px-to-viewport': {
+        //     viewportWidth: 750, // 视窗的宽度，以像素为单位
+        //     unitPrecision: 3, // 转换后的视窗单位保留的小数位数
+        //     viewportUnit: 'vw', // 转换的视窗单位
+        //     selectorBlackList: ['.ignore'], // 需要忽略的CSS选择器，不进行转换
+        //     minPixelValue: 1, // 小于或等于1px的像素不进行转换
+        //     mediaQuery: false // 是否在媒体查询中转换
+        // }
+    }
+}
